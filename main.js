496803309970fd70ebf4899064ac9502d6a8e0cb
TJ <tonyjsouthern@gmail.com>
2018-08-29 04:37:24 -0700

    Made css mobile responsive

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname + '/public'))
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
